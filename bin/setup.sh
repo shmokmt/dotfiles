be7086e02ac1457bf9ff1f89391f6ded29c1602b
shmokmt <32533860+shmokmt@users.noreply.github.com>
2023-12-25 20:49:12 +0900

    Add mitamae's installer

---
+++ bin/setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+mitamae_version="1.14.1"
+mitamae_cache="mitamae-${mitamae_version}"
+
+if [ -f "bin/${mitamae_cache}" ]; then exit; fi
+
+case "$(uname)" in
+  "Linux")
+    mitamae_bin="mitamae-x86_64-linux"
+    ;;
+ "Darwin")
+    mitamae_bin="mitamae-aarch64-darwin"
+    ;;
+  *)
+    echo "unexpected uname: $(uname)"
+    exit 1
+    ;;
+esac
+
+curl -o "bin/${mitamae_cache}" -fL "https://github.com/itamae-kitchen/mitamae/releases/download/v${mitamae_version}/${mitamae_bin}"
+
+chmod +x "bin/${mitamae_cache}"
+ln -sf "${mitamae_cache}" bin/mitamae
