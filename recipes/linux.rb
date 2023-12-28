d39899c4bba99dc8fce33e2f0b804d4b3e8e53fe
shmokmt <32533860+shmokmt@users.noreply.github.com>
2023-12-28 14:48:35 +0900

    Add starship & zshrc for linux

--- recipes/linux.rb
+++ recipes/linux.rb
@@ -1,4 +1,7 @@
 package 'vim'
 package 'peco'
 package 'zsh'
+package 'git'
 execute "curl -sS https://starship.rs/install.sh | sh -s -- --yes"
+execute "mkdir -p ~/.config; cp config/starship.toml ~/.config/starship.toml"
+execute "cp config/.zshrc ~/.zshrc"
