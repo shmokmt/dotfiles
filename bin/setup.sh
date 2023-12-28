86cc4dd273a6c11d42c640e18014ffc235f1af2b
shmokmt <32533860+shmokmt@users.noreply.github.com>
2023-12-28 13:46:47 +0900

    Supports for Linux aarch64

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,11 +7,14 @@ mitamae_cache="mitamae-${mitamae_version}"
 
 if [ -f "bin/${mitamae_cache}" ]; then exit; fi
 
-case "$(uname)" in
-  "Linux")
+case "$(uname)/$(uname -m)" in
+  "Linux/x86_64")
     mitamae_bin="mitamae-x86_64-linux"
     ;;
- "Darwin")
+  "Linux/aarch64" )
+    mitamae_bin="mitamae-aarch64-linux"
+    ;;
+ "Darwin/arm64")
     mitamae_bin="mitamae-aarch64-darwin"
     ;;
   *)
